13e323a89036bab1f9e28548e577d0bf3316666b
proponline <proponline@ubuntu.members.linode.com>
2020-02-07 21:47:58 +0000

    secure it up

--- public/frontend/services/settings.js
+++ public/frontend/services/settings.js
@@ -4,7 +4,7 @@ angular.module('iotgo')
   var host = $location.host() + ':' + $location.port();
 
   return {
-    httpServer: 'http://' + host,
-    websocketServer: 'ws://' + host
+    httpServer: 'https://' + host,
+    websocketServer: 'wss://' + host
   };
 } ]);
