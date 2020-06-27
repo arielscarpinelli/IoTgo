f9376106f56a7e4427b81173dc8c9638c6018ea8
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2020-06-27 19:53:05 -0300

    unlimit http access to protocol

--- routes/http.js
+++ routes/http.js
@@ -7,12 +7,6 @@ const asyncHandler = require('express-async-handler');
 
 module.exports = asyncHandler(async function (req, res) {
 
-  if (req.header('Host') !== config.host ||
-      req.header('Content-Type') !== 'application/json') {
-    res.status(400).end();
-    return;
-  }
-
   try {
 	  const resBody = await protocol.postRequest(req.body);
 	  res.send(resBody);
